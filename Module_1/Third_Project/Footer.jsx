dc12d8d0cb360e544ce912f6c364ee2134b93e16
Henry Possajennikov <Henryposs128@gmail.com>
2025-10-03 20:13:10 +1000

    Add files via upload

---
+++ Module_1/Third_Project/Footer.jsx
@@ -0,0 +1,13 @@
+    import React from 'react';
+    const Footer = () => {
+            return (
+        <section>
+            <div>
+                <footer>
+                    <p>Event Planner Organization. All rights reserved.</p>
+                </footer>
+            </div>
+        </section>
+    )}
+
+export default Footer
